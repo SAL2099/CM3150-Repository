7c7dafc7ca86d05a74020b580f6e09290c559c3c
Stuart <s.lancaster@rgu.ac.uk>
2026-07-24 18:48:26 +0100

    PowerCell completed

---
+++ Assets/Scripts/PowerBox.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PowerBox : MonoBehaviour
+{
+    public bool fullPower;
+    public GameObject regularLighting;
+    public GameObject emergencyLighting;
+    public GameObject staticPowerCell;
+
+    public void OnTriggerEnter(Collision entity)
+    {
+        if(entity.gameObject.tag == "PowerCell")
+        {
+            Destroy(entity.gameObject);
+            staticPowerCell.SetActive(true);
+            fullPower = true;
+            emergencyLighting.SetActive(false);
+            regularLighting.SetActive(true);
+        }
+    }
+}
