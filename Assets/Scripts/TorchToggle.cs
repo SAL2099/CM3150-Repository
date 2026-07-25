0a9495a6152a2341d80d412878a0adfaa748e87a
Stuart <s.lancaster@rgu.ac.uk>
2026-07-25 15:36:26 +0100

    The audio update part 1

--- Assets/Scripts/TorchToggle.cs
+++ Assets/Scripts/TorchToggle.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class TorchToggle : MonoBehaviour
 {
     public GameObject torch;
+    public AudioSource audioSource;
+    public AudioClip soundEffect;
 
   public void TorchOn()
     {
         torch.SetActive(true);
+        audioSource.PlayOneShot(soundEffect);
     }
 
     public void TorchOff() 
     {
         torch.SetActive(false);
+        audioSource.PlayOneShot(soundEffect);
     }
 }
